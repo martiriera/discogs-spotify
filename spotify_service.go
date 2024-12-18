eee3df2d520e0485432551e37f7ce2d655c8f4da
Martí Riera <martirieravila@gmail.com>
2024-12-18 18:21:32 +0100

    wip

--- spotify_service.go
+++ spotify_service.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/url"
 	"os"
@@ -33,15 +34,32 @@ type SpotifyAuthResponse struct {
 	ExpiresIn   int    `json:"expires_in"`
 }
 
-// func (s *HttpSpotifyService) GetAlbumId(album string, artist string) (string, error) {
-// 	const uri = "https://api.spotify.com/v1/search?type=album"
-// 	req, err := http.NewRequest(http.MethodGet, uri, nil)
-// 	if err != nil {
-// 		return "", err
-// 	}
-// }
+func (s *HttpSpotifyService) GetAlbumId(album string, artist string) (*SpotifyResponse, error) {
+	const path = "https://api.spotify.com/v1/search"
+	query := "album:" + album + " artist:" + artist + " type:album"
+	route := path + "?q=" + url.QueryEscape(query)
+	req, err := http.NewRequest(http.MethodGet, route, nil)
+	if err != nil {
+		return nil, err
+	}
+	token, err := s.getAccessToken()
+	if err != nil {
+		return nil, fmt.Errorf("error getting Spotify access token: %v", err)
+	}
+	req.Header.Set("Authorization", "Bearer "+token)
+	resp, err := s.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error getting Spotify album id: %v", err)
+	}
+	defer resp.Body.Close()
+	var response SpotifyResponse
+	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
+		return nil, fmt.Errorf("unable to parse Spotify response, %v ", err)
+	}
+	return &response, nil
+}
 
-func (s *HttpSpotifyService) GetAccessToken() (string, error) {
+func (s *HttpSpotifyService) getAccessToken() (string, error) {
 	const authUrl = "https://accounts.spotify.com/api/token"
 	data := url.Values{}
 	data.Set("grant_type", "client_credentials")
@@ -54,10 +72,12 @@ func (s *HttpSpotifyService) GetAccessToken() (string, error) {
 	var response SpotifyAuthResponse
 	resp, err := s.client.Do(r)
 	if err != nil {
+		// TODO: explain error
 		return "", err
 	}
 	defer resp.Body.Close()
 	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
+		// TODO: explain error
 		return "", err
 	}
 	return response.AccessToken, nil
