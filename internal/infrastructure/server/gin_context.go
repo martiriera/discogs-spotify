3d3706274b4ade2514473771480e6068edbc3fd9
Martí Riera <martirieravila@gmail.com>
2025-11-03 14:12:23 +0100

    refactor: rename unused variables

--- internal/infrastructure/server/gin_context.go
+++ internal/infrastructure/server/gin_context.go
@@ -28,7 +28,7 @@ func NewGinContextProvider() *GinContextProvider {
 	return &GinContextProvider{}
 }
 
-func (p *GinContextProvider) GetToken(ctx context.Context) (*oauth2.Token, error) {
+func (_ *GinContextProvider) GetToken(ctx context.Context) (*oauth2.Token, error) {
 	ginCtx, ok := ctx.(*gin.Context)
 	if !ok {
 		return nil, fmt.Errorf("context is not a gin.Context")
@@ -47,7 +47,7 @@ func (p *GinContextProvider) GetToken(ctx context.Context) (*oauth2.Token, error
 	return token, nil
 }
 
-func (p *GinContextProvider) GetUserID(ctx context.Context) (string, error) {
+func (_ *GinContextProvider) GetUserID(ctx context.Context) (string, error) {
 	ginCtx, ok := ctx.(*gin.Context)
 	if !ok {
 		return "", fmt.Errorf("context is not a gin.Context")
